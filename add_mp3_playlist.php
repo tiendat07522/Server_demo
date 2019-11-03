a3702be6fb2974a8de257ee1e55b3b62a3a0e936
tiendat07522 <tiendat07522@users.noreply.github.com>
2019-11-03 17:34:59 +0700

    update 3

--- add_mp3_playlist.php
+++ add_mp3_playlist.php
@@ -3,11 +3,13 @@
 $playlist_id=$_POST['playlist_id'];
 $mp3_id=$_POST['mp3_id'];
 $data=array();
-	if(strlen($username)>0 && strlen($password)>0){
+	
         $query = "INSERT INTO `playlist_details` (playlist_id, mp3_id) VALUES ('$playlist_id', '$mp3_id')";
         $result = mysqli_query($con,$query);
-     $data[]=array('status'=>200);
+     if($result){
+         $data[]=array('status'=>200);
      	print json_encode($data);
+     }
     }else{
         $data[]=array('status'=>404);
         	print json_encode($data);
--- create_playlist.php
+++ create_playlist.php
@@ -3,11 +3,12 @@
 $playlist_name=$_POST['playlist_name'];
 $users_id=$_POST['users_id'];
 $data=array();
-	if(strlen($username)>0 && strlen($password)>0){
         $query = "INSERT INTO `playlist` (playlist_name, users_id,count) VALUES ('$playlist_name', '$users_id',NULL)";
         $result = mysqli_query($con,$query);
-     $data[]=array('status'=>200);
+   if($result){
+         $data[]=array('status'=>200);
      	print json_encode($data);
+   }
     }else{
         $data[]=array('status'=>404);
         	print json_encode($data);
