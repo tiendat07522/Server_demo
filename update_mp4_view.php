072bc5792b6b2e2f01dc1238f3519ad8b547bf79
tiendat07522 <tiendat07522@users.noreply.github.com>
2019-11-03 09:57:57 +0700

    update view

--- update_mp4_view.php
+++ update_mp4_view.php
@@ -1,16 +1,16 @@
  <?php require_once 'config.php';?>
  <?php 
 
-        $mp3_id = $_POST['mp3_id'];
-    // get view from mp3
-    $func="SELECT * FROM `mp3`";
+        $mp4_id = $_POST['mp4_id'];
+    // get view from mp4
+    $func="SELECT * FROM `mp4`";
     $rest=mysqli_query($con,$func);
     $row = mysqli_fetch_assoc($rest);
     $view=$row['view'];
     $view_count = $view+1;
-        $query="UPDATE mp3
+        $query="UPDATE mp4
                 SET view=$view_count
-                WHERE mp3_id=$mp3_id";
+                WHERE mp4_id=$mp4_id";
         if($result = mysqli_query($con,$query)){
              $data[]=array('status'=>200);
      	print json_encode($data);
